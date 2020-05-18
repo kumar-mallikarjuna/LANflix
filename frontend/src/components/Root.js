52645c72a97c88c7ecf7802437a59050f4d05ebf
Kumar Mallikarjuna <kumarmallikarjuna1@gmail.com>
2020-05-18 22:43:57 +0530

    Added frontend base

---
+++ frontend/src/components/Root.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Root extends React.Component {
+	render() {
+		return this.props.children;
+	}
+}
+
+export default Root;
---
+++ frontend/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Root from "./components/Root";
+import { render } from "react-dom";
+
+const root = document.getElementById("wrapper");
+render(<Root>Hello!</Root>, root);
